100b48565b7495af5df76c3a4fdf569aef7c208b
Ken <yash20004x17@gmail.com>
2024-12-06 21:26:52 +0530

    Working on Form Validator Project

---
+++ Project_01/script.js
@@ -0,0 +1,7 @@
+// // const min = 1
+// // const max = 6
+
+// // console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+// let myDate = new Date()
+// console.log(myDate.toLocaleDateString());
