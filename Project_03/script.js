71a4558a6423b507d5e7e9a459af97d41c6e1a8c
Ken <yash20004x17@gmail.com>
2024-12-20 21:12:06 +0530

    Working on Movie Seat Booking Project

---
+++ Project_03/script.js
@@ -0,0 +1,8 @@
+const optionValue = parseInt(document.querySelector('#movie-selector'));
+optionValue.addEventListener('click', (e) => {
+    // const value = parseInt(document.querySelector('option').value);
+    // console.log(e.target.value);
+    console.log(optionValue);
+    
+})
+
